ba1e501b56ec1d13ab897649076dcec14343ab78
Andrian Mamchuk <andrian.mamchuk@gmail.com>
2024-01-03 22:19:21 +0200

    Fixed bugs

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -16,29 +16,46 @@ populateForm();
 function onFormSubmit(event) {
   event.preventDefault();
 
-  if (formData.email === '' || formData.message === '') {
+  if (!validateForm()) {
     return alert('All form fields must be filled in');
   }
 
-  console.log(formData);
-  formData.email = '';
-  formData.message = '';
-  event.currentTarget.reset();
+  if (isFormDataNotEmpty()) {
+    console.log(formData);
+  }
+
+  resetForm();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
 function onFormInput(event) {
-  formData[event.target.name] = event.target.value;
+  const { name, value } = event.target;
+  formData[name] = value.trim();
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateForm() {
-  const savedInputEl = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  const savedFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-  if (savedInputEl) {
-    refs.input.value = savedInputEl.email || '';
-    refs.textarea.value = savedInputEl.message || '';
-    formData.email = savedInputEl.email || '';
-    formData.message = savedInputEl.message || '';
+  if (savedFormData) {
+    const { email, message } = savedFormData;
+    refs.input.value = email || '';
+    refs.textarea.value = message || '';
+    formData.email = email || '';
+    formData.message = message || '';
   }
+}
+
+function validateForm() {
+  return Object.values(formData).every(value => value.trim() !== '');
+}
+
+function resetForm() {
+  refs.form.reset();
+  formData.email = '';
+  formData.message = '';
+}
+
+function isFormDataNotEmpty() {
+  return Object.keys(formData).length > 0;
 }
